22cf53bf221fc80980ba007165d23b11f3f7becd
thien2218 <thien1367526@gmail.com>
2023-06-22 22:54:20 +0700

    Initialize project

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module, ValidationPipe } from "@nestjs/common";
+import { APP_PIPE } from "@nestjs/core";
+
+@Module({
+   imports: [],
+   controllers: [],
+   providers: [
+      {
+         provide: APP_PIPE,
+         useClass: ValidationPipe
+      }
+   ]
+})
+export class AppModule {}
